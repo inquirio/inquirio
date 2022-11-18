6513cfc22674a88a477b560d3137e79cc0ebb531
Keelen Fisher <keelencarrera@gmail.com>
2022-11-18 16:26:36 -0600

    adding design to the Home page

---
+++ Components/Login/login.js
@@ -0,0 +1,35 @@
+import { If, Then, Else } from 'react-if';
+import { Button, Group, TextInput } from '@mantine/core';
+
+
+const Login = () => {
+ 
+
+  return (
+    <>
+      <If>
+        <Then>
+          <Button color="red" >Log Out</Button>
+        </Then>
+        <Else>
+          <form >
+            <Group style={{ width: '500px' }}>
+              <TextInput
+                name="username"
+                placeholder="Username"
+              />
+              <TextInput
+                name="password"
+                type="password"
+                placeholder="Password"
+              />
+              <Button color="green" type="submit" >Log In</Button>
+            </Group>
+          </form>
+        </Else>
+      </If>
+    </>
+  );
+};
+
+export default Login;
