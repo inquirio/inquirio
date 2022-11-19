2659a43b53912b0b7de652392a45735ea3355146
Stephanie Hill <stephni.hill@gmail.com>
2022-11-18 16:34:15 -0800

    not pretty but we have a file

---
+++ Data/parse.js
@@ -0,0 +1,33 @@
+const csvParser = require('csv-parser');
+const fs = require('fs')
+
+const courseraFile = 'coursera.csv'
+
+const results = [];
+
+// fs.createReadStream('coursera.csv')
+// .pipe(csv({}))
+// .on('data', (data) => results.push(data))
+// .on('end', () => {
+  
+//   console.log(results);
+// })
+
+fs.createReadStream(courseraFile, {encoding: 'utf-8'})
+.pipe(csvParser(Array[String]))
+.on('data', (data) => results.push(data))
+.on('end', () => { 
+  const stringifyResults = JSON.stringify(results)
+  console.log(results);
+  fs.writeFile('coursera.json', stringifyResults, (error) => {
+    if(error){
+      console.error(error);
+    } else {
+      console.log('File Written Successfully');
+      console.log(fs.readFileSync('coursera.csv', 'utf8'))
+  
+    }
+  })
+})
+
+
