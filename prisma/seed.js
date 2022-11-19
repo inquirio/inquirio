3ccda18454ce40df49f1c5bc2a742873459fde22
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-11-19 23:28:24 +0000

    Add udacity and future-learn to database seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 const csvParser = require('csv-parser');
 const courseraFile = './Data/coursera.csv'
 const edxFile = './Data/edX.csv'
+const futureLearnFile = './Data/future-learn.csv'
+const udacityFile = './Data/udacity.csv'
 const prisma = require('.');
 
 async function translate() {
@@ -51,6 +53,51 @@ async function translate() {
         console.log(data.name)
       }
     })
+
+  const futureLearn = []
+  fs.createReadStream(futureLearnFile, { encoding: 'utf-8' })
+    .pipe(csvParser(Array[String]))
+    .on('data', (data) => futureLearn.push(data))
+    .on('end', async () => {
+      for (let course of futureLearn) {
+        const data = {
+          url: course['link-href'],
+          name: course.name,
+          provider: course.provider,
+          category: /(?<=subjects\/)(.+)(?=-courses)/.exec(course['web-scraper-start-url'])[0].replaceAll('-', ' '),
+          image: course['image-src'],
+        }
+        await prisma.course.upsert({
+          where: { url: data.url },
+          update: data,
+          create: data,
+        })
+        console.log(data.name)
+      }
+    })
+
+  const udacity = []
+  fs.createReadStream(udacityFile, { encoding: 'utf-8' })
+    .pipe(csvParser(Array[String]))
+    .on('data', (data) => udacity.push(data))
+    .on('end', async () => {
+      for (let course of udacity) {
+        const data = {
+          url: course['link-href'],
+          name: course.name,
+          provider: 'Udacity',
+          category: /(?<=field=)(.+)/.exec(course['web-scraper-start-url'])[0].replaceAll('-', ' '),
+          image: course['image-src'],
+        }
+        await prisma.course.upsert({
+          where: { url: data.url },
+          update: data,
+          create: data,
+        })
+        console.log(data.name)
+      }
+    })
+
 }
 
 translate()
