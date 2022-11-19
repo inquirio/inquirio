397e09abc52f13175df5b936044d180dcde36138
Stephanie Hill <stephni.hill@gmail.com>
2022-11-18 17:02:44 -0800

    edx json

--- Data/parse.js
+++ Data/parse.js
@@ -2,16 +2,12 @@ const csvParser = require('csv-parser');
 const fs = require('fs')
 
 const courseraFile = 'coursera.csv'
+const edXFile = 'edX.csv'
 
 const results = [];
+const edXResults = [];
 
-// fs.createReadStream('coursera.csv')
-// .pipe(csv({}))
-// .on('data', (data) => results.push(data))
-// .on('end', () => {
-  
-//   console.log(results);
-// })
+
 
 fs.createReadStream(courseraFile, {encoding: 'utf-8'})
 .pipe(csvParser(Array[String]))
@@ -30,4 +26,19 @@ fs.createReadStream(courseraFile, {encoding: 'utf-8'})
   })
 })
 
-
+fs.createReadStream(edXFile, {encoding: 'utf-8'})
+.pipe(csvParser(Array[String]))
+.on('data', (data) => edXResults.push(data))
+.on('end', () => { 
+  const stringifyResults = JSON.stringify(edXResults, null, 2)
+  console.log(edXResults);
+  fs.writeFile('edX.json', stringifyResults, (error) => {
+    if(error){
+      console.error(error);
+    } else {
+      console.log('File Written Successfully');
+      console.log(fs.readFileSync('edX.csv', 'utf8'))
+  
+    }
+  })
+})
