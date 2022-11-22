190d2b107d025a5fed6bea53713951a14b91f83e
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-11-22 23:46:46 +0000

    Update enrollments page to new styling

--- pages/enrollments.js
+++ pages/enrollments.js
@@ -1,10 +1,9 @@
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea, CardActions, Pagination } from '@mui/material';
+import { Box, Card, CardActions, CardActionArea, CardContent, CardMedia, Grid, IconButton, Pagination, Typography } from '@mui/material';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import SearchIcon from '@mui/icons-material/Search';
 import { useRouter } from 'next/router';
 import Search from '../Components/Search/Search';
+import styles from '../styles/Courses.module.css';
 import { getEnrollment } from '../lib/enrollment';
 import { getSession, withPageAuthRequired } from '@auth0/nextjs-auth0';
 
@@ -17,7 +16,6 @@ export const getServerSideProps = withPageAuthRequired({
   }
 })
 
-
 export default function Enrollments({ data }) {
   const router = useRouter();
   const setPage = value => {
@@ -26,35 +24,78 @@ export default function Enrollments({ data }) {
   }
 
   return (
-    <>
+    <Box className={styles.parentBox}>
       <Search enrollment={true} />
-      {data.courses && data.courses.map((enrollment, index) => (
-        <Card
-          key={`course-${index}`}
-          sx={{ maxWidth: 345 }}>
-          <CardActionArea href={enrollment.Course.url} >
-            <CardMedia
-              component="img"
-              height="140"
-              image={enrollment.Course.image}
-              alt={enrollment.Course.name}
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {enrollment.Course.name}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      ))}
-      {data.totalPages && <Pagination
-        onChange={(e, value) => setPage(value)}
-        page={parseInt(data.query.page)}
-        count={data.totalPages}
-        variant="outlined"
-      />}
+      <Grid
+        container spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 12, sm: 16, md: 20 }}
+        sx={{
+          margin: '10%',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '50%'
+        }}
+      >
+        {
+          data.courses && data.courses.map((enrollment, index) => (
+            <Grid
+              key={`course-${index}`}
+              component="div"
+              item xs={2} sm={4} md={4}
+            >
+              <Card
+                className={styles.card}
+                sx={{
+                  height: '35vh',
+                }}
+              >
+                <CardActionArea href={enrollment.Course.url} >
+                  <CardMedia
+                    className={styles.cardImg}
+                    component="img"
+                    image={enrollment.Course.image}
+                    alt={enrollment.Course.name}
+                  />
+                  <CardContent className={styles.cardBody}>
+                    <Typography
+                      className={styles.cardHeader}
+                      component="div">
+                      {enrollment.Course.name}
+                    </Typography>
+                    <Typography
+                      className={styles.cardText}
+                      variant="body2"
+                      color="text.secondary"
+                    >
+                      {enrollment.Course.provider}
+                    </Typography>
+                  </CardContent>
 
-    </>
-
+                  <CardContent className={styles.cardFoot} >
+                    <CardActions >
+                      <IconButton
+                        className={styles.heartButton}
+                        aria-label="add to favorites">
+                        <FavoriteIcon />
+                      </IconButton>
+                    </CardActions>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+        {data.totalPages &&
+          <Pagination
+            onChange={(e, value) => setPage(value)}
+            page={parseInt(data.query.page)}
+            count={parseInt(data.totalPages)}
+            className={styles.Pagination}
+            size="large"
+            color="primary"
+            variant="outlined"
+            shape="rounded"
+          />}
+      </Grid>
+    </Box>
   )
 }
