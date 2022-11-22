75975a7224f4c7ed3254a98aa57b98988ad2da76
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-11-22 03:39:18 +0000

    Create or update user on login

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,3 +1,24 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth, handleCallback, session } from '@auth0/nextjs-auth0';
 
-export default handleAuth();
+const afterCallback = async (req, res, session) => {
+  const data = {
+    name: session.user.name,
+    email: session.user.email,
+  }
+  await prisma.User.upsert({
+    where: { email: session.user.email },
+    update: data,
+    create: data
+  });
+
+}
+
+export default handleAuth({
+  async callback(req, res) {
+    try {
+      await handleCallback(req, res, { afterCallback })
+    } catch (error) {
+      res.status(error.status || 500).end(error.message);
+    }
+  }
+});
