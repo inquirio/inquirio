7715b9fec0bf691d309a09829b7a8a3aa1a9e3c0
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-11-22 03:40:22 +0000

    Remove old user creation code

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,22 +1,6 @@
 import prisma from '../../prisma/index'
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const { name, email } = req.body;
-    try {
-      const user = await prisma.User.create({
-        data: {
-          name: name,
-          email: email,
-        }
-      });
-      res.status(201).json({ user });
-
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
   if (req.method === 'GET') {
     try {
       const result = await prisma.user.findMany();
