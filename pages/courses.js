bb66ec11421fe0894bb81fd1837e9e94ebbee4f6
Stephanie Hill <stephni.hill@gmail.com>
2022-11-21 22:46:01 -0800

    search

--- pages/courses.js
+++ pages/courses.js
@@ -1,12 +1,13 @@
 
 import { useState, useEffect } from 'react'
-
+// import Category from '../Components/Category/category';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea, CardActions, Pagination } from '@mui/material';
-
+import Grid from '@mui/material/Grid';
+import { CardActionArea, Pagination } from '@mui/material';
+import { FormGroup, TextField, Button } from '@mui/material';
 export default function Courses() {
 
   const [data, setData] = useState([]);
@@ -33,32 +34,49 @@ export default function Courses() {
     dbQuery()
   }, [page])
 
-  // {course.url}
   return (
-
     <>
-      {data.courses && data.courses.map((course, index) => (
-        <Card
-          key={`course-${index}`}
-          sx={{ maxWidth: 345 }}>
-          <CardActionArea href={course.url} >
-            <CardMedia
-              component="img"
-              height="140"
-              image={course.image}
-              alt={course.name}
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {course.name}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      ))}
-      {data.totalPages && <Pagination onChange={(e, value) => setPage(value)} count={data.totalPages} />}
-
+    
+    <FormGroup >
+        <TextField onChange={(e) => setSearch(e.target.value)} id="filled-basic" label="By Course" variant="filled" />
+        <TextField onChange={(e) => setCategory(e.target.value)} id="filled-basic" label="By Category" variant="filled" />
+        <TextField onChange={(e) => setProvider(e.target.value)} id="filled-basic" label="By Provider" variant="filled" />
+          <Button onClick={dbQuery} >Search</Button>
+        </FormGroup>
+      <Grid
+        container spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 8, sm: 12, md: 16 }}
+      >
+        {
+          data.courses && data.courses.map((course, index) => (
+            <Grid item xs={2} sm={4} md={4} >
+              <Card key={`course-${index}`} >
+                <CardActionArea href={course.url} >
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={course.image}
+                    alt={course.name}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {course.name}
+                    </Typography>
+                    <Typography gutterBottom component="div">
+                      {course.provider}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+        {data.totalPages && <Pagination onChange={(e, value) => setPage(value)} count={data.totalPages} />}
+      </Grid>
+    
     </>
 
   )
+
 }
+
+
--- prisma/index.ts
+++ prisma/index.ts
@@ -8,4 +8,5 @@ export const prisma =
   global.prisma ||
   new PrismaClient()
 
+/* It's a way to make sure that the prisma client is only instantiated once. */
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma
