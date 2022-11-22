f826d93c724083dd20895863970100fae3718c7d
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-11-22 04:02:52 +0000

    Bugfix for post enrollment

--- pages/api/enrollment.js
+++ pages/api/enrollment.js
@@ -1,22 +1,23 @@
-import prisma from '../../prisma/index'
+import { prisma } from '../../prisma/index'
 
 export default async function handler(req, res) {
-  if(req.method === 'POST'){
-    const {courseId, userId} = req.body;
-    try{
+  if (req.method === 'POST') {
+    const { courseId, userEmail } = req.body;
+    try {
       const enrollment = await prisma.enrollment.create({
         data: {
-          user: {
-            connect: { id: userId },
+          User: {
+            connect: { email: userEmail },
           },
-          course: {
+          Course: {
             connect: { id: courseId },
           },
         },
       })
-      res.status(200).json({enrollment});
+      console.log(enrollment)
+      res.status(200).json({ enrollment });
 
-    } catch(e) {
+    } catch (e) {
       console.log(e);
     }
   }
