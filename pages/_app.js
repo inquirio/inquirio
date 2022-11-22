1fa26bc8641789c5cb9c0bb19d1e1a5212bfd31e
Brandon Pitts <Brandomoki.bp@gmail.com>
2022-11-22 22:56:18 +0000

    created navbar with profile data

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,16 @@
-import '../styles/globals.css'
 import React from 'react';
 import { UserProvider } from '@auth0/nextjs-auth0';
+import '../styles/globals.css'
 import '../styles/transition.css';
 // import '../styles/Settings.module.css';
 
 import Transition from '../Components/Transition/transition';
 
 export default function App({ Component, pageProps }) {
-const { user } = pageProps;
+
 
   return (
-    <UserProvider user={ user }>
+    <UserProvider >
       <Transition>
         <Component {...pageProps} />
       </Transition>
